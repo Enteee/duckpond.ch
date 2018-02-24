2ef9831146bee7fb2d2b3f3070e473d50d190588
Ente <ducksource@duckpond.ch>
2018-02-24 23:27:03 +0100

    remove hook

--- _env/dehydrated/etc/hook.sh
+++ _env/dehydrated/etc/hook.sh
@@ -1,19 +1,7 @@
 #!/usr/bin/env bash
-set -ex
+set -x
 
 event="${1}" && shift
-
-function link(){(
-  local file="${1}" && shift
-  local to="${1}" && shift
-
-  dir=$(dirname "${file}")
-  from=$(basename "${file}")
-
-  cd "${dir}"
-  ln --force --symbolic \
-    "${from}" "${to}"
-)}
 
 case "${event}" in
   deploy_cert|unchanged_cert)
@@ -21,9 +9,6 @@ case "${event}" in
     privkey="${1}" && shift
     cert="${1}" && shift
     fullchain="${1}" && shift
-    link "${privkey}" "privkey.pem"
-    link "${cert}" "cert.pem"
-    link "${fullchain}" "fullchain.pem"
   ;;
   *) ;;
 esac
