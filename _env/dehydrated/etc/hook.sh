8039e3cb257dd3ed5060de7bbb77f2c1f76ae1a1
Ente <ducksource@duckpond.ch>
2019-07-10 16:33:48 +0200

    generate dhparams & key

--- _env/dehydrated/etc/hook.sh
+++ _env/dehydrated/etc/hook.sh
@@ -9,6 +9,16 @@ case "${event}" in
     privkey="${1}" && shift
     cert="${1}" && shift
     fullchain="${1}" && shift
+
+    ln \
+      --force \
+      --symbolic \
+      "${privkey}" key.pem
+
+    # generate diffie hellman parameters
+    if [ ! -f "${DH_OUTFILE}" ]; then
+      openssl dhparam -out "${DH_OUTFILE}" 4096
+    fi
   ;;
   *) ;;
 esac
