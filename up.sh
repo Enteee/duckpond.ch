5f6baf30cf65c1e9b2a5ab2edf36a1424ff9093d
Ente <ducksource@duckpond.ch>
2019-07-10 14:29:59 +0200

    encrypt /decrypt

--- up.sh
+++ up.sh
@@ -1,39 +1,101 @@
 #!/usr/bin/env bash
-set -e
+set -euo pipefail
+set -x
 
-DIR=$(dirname $0)
-DEVELOP=false
+PWD="$(pwd)"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-while getopts "dh" opt; do
-  case $opt in
-    d)
-      DEVELOP=true
-      ;;
-    h)
-      cat<<EOF
-$0: run the blog
+DOCKER_COMPOSE="${DIR}/docker-compose.sh"
+
+function usage(){
+cat <<EOF
+up.sh: run the blog
 
 Options:
-  -h    print this help
-  -d    develop mode
+  -h|--help       print this help
+  -d|--develop    develop mode
 EOF
+}
+
+function decrypt(){
+  local in="${1}" && shift
+  local pw="${1}" && shift
+  local out="$(basename --suffix ".gpg" "${in}")"
+
+  echo "${pw}" \
+  | gpg \
+    --decrypt \
+    --batch \
+    --yes \
+    --passphrase-fd 0 \
+    --output "${out}"\
+    "${in}"
+}
+
+function encrypt(){
+  local in="${1}" && shift
+  local pw="${1}" && shift
+  local out="${in}.gpg"
+  echo "${pw}" \
+  | gpg \
+    --batch \
+    --yes \
+    --symmetric \
+    --passphrase-fd 0 \
+    --output "${out}"\
+    "${in}"
+}
+
+develop=false
+encrypt=false
+encrypt_password=""
+decrypt=false
+decrypt_password=""
+
+while [[ $# -gt 0 ]]; do
+  case "${1}" in
+    -d|--develop)
+      DEVELOP=true && shift
+      ;;
+    -h|--help)
+      usage
       exit
       ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
+    -ec|--encrypt)
+      encrypt=true && shift
+      encrypt_password="${1}" && shift
+      ;;
+    -dc|--decrypt)
+      decrypt=true && shift
+      decrypt_password="${1}" && shift
+      ;;
+    ?)
+      echo "Invalid argument: '${1}'" >&2
+      usage >&2
       exit 1
       ;;
   esac
 done
 
+if [ "${encrypt}" == true ];then
+  find
+    "${DIR}" \
+    --name "*.gpg" \
+    --type f \
+    --execdir encrypt "{}" "${encrypt_password}" \;
+fi
+
+
 if [ $DEVELOP = true ]; then
-    exec docker-compose \
+    exec "${DOCKER_COMPOSE}" \
       -f docker-compose.yml \
-      -f docker-compose-develop.yml \
+      -f docker-compose-dev.yml \
+      -f _env/mailcow/docker-compose.yml \
+      --verbose \
       up
 fi
 
-exec docker-compose \
+exec "${DOCKER_COMPOSE}" \
   -f docker-compose.yml \
   -f docker-compose-prod.yml \
   up
