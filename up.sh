6c838da0b780b073bdd3314c6102eeda5571379b
Ente <ducksource@duckpond.ch>
2019-07-10 14:46:28 +0200

    enc & decrypt

--- up.sh
+++ up.sh
@@ -17,10 +17,38 @@ Options:
 EOF
 }
 
+#
+# encrypt out pw
+function encrypt(){
+  local out="${1}" && shift
+  local pw="${1}" && shift
+  local in="$(basename --suffix ".gpg" "${out}")"
+
+  if [ ! -f "${in}" ]; then
+    return
+  fi
+
+  echo "${pw}" \
+  | gpg \
+    --batch \
+    --yes \
+    --symmetric \
+    --passphrase-fd 0 \
+    --output "${out}"\
+    "${in}"
+}
+export -f encrypt
+
+#
+# decrypt in pw
 function decrypt(){
   local in="${1}" && shift
   local pw="${1}" && shift
   local out="$(basename --suffix ".gpg" "${in}")"
+
+  if [ ! -f "${in}" ]; then
+    return
+  fi
 
   echo "${pw}" \
   | gpg \
@@ -31,22 +59,10 @@ function decrypt(){
     --output "${out}"\
     "${in}"
 }
-
-function encrypt(){
-  local in="${1}" && shift
-  local pw="${1}" && shift
-  local out="${in}.gpg"
-  echo "${pw}" \
-  | gpg \
-    --batch \
-    --yes \
-    --symmetric \
-    --passphrase-fd 0 \
-    --output "${out}"\
-    "${in}"
-}
+export -f decrypt
 
 develop=false
+
 encrypt=false
 encrypt_password=""
 decrypt=false
@@ -55,7 +71,7 @@ decrypt_password=""
 while [[ $# -gt 0 ]]; do
   case "${1}" in
     -d|--develop)
-      DEVELOP=true && shift
+      develop=true && shift
       ;;
     -h|--help)
       usage
@@ -63,11 +79,11 @@ while [[ $# -gt 0 ]]; do
       ;;
     -ec|--encrypt)
       encrypt=true && shift
-      encrypt_password="${1}" && shift
+      encrypt_password="${1?missing password}" && shift
       ;;
     -dc|--decrypt)
       decrypt=true && shift
-      decrypt_password="${1}" && shift
+      decrypt_password="${1?missing password}" && shift
       ;;
     ?)
       echo "Invalid argument: '${1}'" >&2
@@ -78,21 +94,29 @@ while [[ $# -gt 0 ]]; do
 done
 
 if [ "${encrypt}" == true ];then
-  find
+  find \
     "${DIR}" \
-    --name "*.gpg" \
-    --type f \
-    --execdir encrypt "{}" "${encrypt_password}" \;
+    -type f \
+    -name "*.gpg" \
+    -execdir bash -c "encrypt \"{}\" \"${encrypt_password}\"" \;
 fi
 
+if [ "${decrypt}" == true ];then
+  find \
+    "${DIR}" \
+    -type f \
+    -name "*.gpg" \
+    -execdir bash -c "decrypt \"{}\" \"${decrypt_password}\"" \;
+fi
 
-if [ $DEVELOP = true ]; then
+if [ "$develop" = true ]; then
     exec "${DOCKER_COMPOSE}" \
       -f docker-compose.yml \
       -f docker-compose-dev.yml \
       -f _env/mailcow/docker-compose.yml \
       --verbose \
       up
+    exit
 fi
 
 exec "${DOCKER_COMPOSE}" \
