469da2b9e47a53b63b2918c6320fc37bb2dd4512
Ente <ducksource@duckpond.ch>
2021-03-16 23:03:31 +0100

    more space

--- _env/cryptpad/config/config.js
+++ _env/cryptpad/config/config.js
@@ -159,6 +159,7 @@ module.exports = {
      *  hint: 50MB is 50 * 1024 * 1024
      */
     //defaultStorageLimit: 50 * 1024 * 1024,
+    defaultStorageLimit: 100 * 1024 * 1024,
 
 
     /* =====================
@@ -215,6 +216,7 @@ module.exports = {
      *  defaults to 20MB if no value is provided
      */
     //maxUploadSize: 20 * 1024 * 1024,
+    maxUploadSize: 50 * 1024 * 1024,
 
     /*
      *  CryptPad allows administrators to give custom limits to their friends.
