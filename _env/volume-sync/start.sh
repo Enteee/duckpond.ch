21d1eb0f9ddca363c2d97eab9163398509894aee
Ente <ducksource@duckpond.ch>
2021-03-25 08:40:22 +0100

    run as root

--- _env/volume-sync/start.sh
+++ _env/volume-sync/start.sh
@@ -6,43 +6,15 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+# Environment
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-# Returns a list of container ids to pause
-get_pause_containers(){
-  docker ps \
-    --filter "label=volume-sync_pause=true" \
-    --format '{{.ID}}'
-}
-export PAUSE_CONTAINERS
-PAUSE_CONTAINERS="$(get_pause_containers)"
+export VOLUME_SYNC_SLEEP_TIME="${VOLUME_SYNC_SLEEP_TIME:-4h}"
 
 while true; do
-  if [ -n "${PAUSE_CONTAINERS}" ]; then
-    echo "Pause containers"
-    # shellcheck disable=SC2086
-    docker pause ${PAUSE_CONTAINERS}
-  fi
-
   echo "Backup"
-  (
-    cd "${SERVICE_USER_HOME}"
-    HOME="${SERVICE_USER_HOME}"
-    su \
-      --preserve-environment \
-      --shell "/bin/bash" \
-      --group "${SERVICE_GROUP}" \
-      "${SERVICE_USER}" \
-      "${DIR}/backup.sh"
-  )
-
-  if [ -n "${PAUSE_CONTAINERS}" ]; then
-    echo "UnPause containers"
-    # shellcheck disable=SC2086
-    docker unpause ${PAUSE_CONTAINERS}
-  fi
+  "${DIR}/backup.sh"
 
   echo "Sleeping"
   # Sleep
-  sleep 1h
+  sleep "${VOLUME_SYNC_SLEEP_TIME}"
 done
